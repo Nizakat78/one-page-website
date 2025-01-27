b63336d1c4f5524610f4d815bb54aa18975f0794
unknown <nizakatalilaghari7@gmail.com>
2025-01-27 16:21:39 +0500

    some change

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["one-page-website-gules.vercel.app"], // Add your domain here
+  },
 };
 
 export default nextConfig;
